1ea1c2c3acfe821b19096ed265272b80a9942583
Vishal Neharkar <neharkar.vishal@gmail.com>
2019-10-16 17:03:23 +0530

    Implemented Mallard Duck.

---
+++ src/main/java/com/simulator/MiniDuckSimulator.java
@@ -0,0 +1,13 @@
+package main.java.com.simulator;
+
+import main.java.com.simulator.ducks.Duck;
+import main.java.com.simulator.ducks.MallardDuck;
+
+public class MiniDuckSimulator {
+
+  public static void main(String[] args) {
+    Duck mallard = new MallardDuck();
+
+    mallard.display();
+  }
+}
---
+++ src/main/java/com/simulator/ducks/MallardDuck.java
@@ -0,0 +1,11 @@
+package main.java.com.simulator.ducks;
+
+public class MallardDuck extends Duck {
+  public MallardDuck() {
+  }
+
+  @Override
+  public void display() {
+    System.out.println("I'm a real Mallard duck");
+  }
+}
